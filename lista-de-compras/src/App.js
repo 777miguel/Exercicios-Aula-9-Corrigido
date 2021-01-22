952806a3df986265987a53012c5901a1a039a088
Lincoln Miguel <lincolncampinas@hotmail.com>
2021-01-22 11:26:41 -0300

    Exercicio corrigido

---
+++ lista-de-compras/src/App.js
@@ -0,0 +1,11 @@
+import ListaDeCompras from './ListaDeCompras'
+
+function App() {
+  return (
+    <div className="App">
+      <ListaDeCompras />
+    </div>
+  );
+}
+
+export default App;
---
+++ lista-de-compras/src/ListaDeCompras.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Item from './Item'
+
+function ListaDeCompras() {
+    return(
+      <>  <Item nome="arroz" />
+        <Item nome="feijao" />
+        <Item nome="banana" />
+        <Item nome="Pera" />
+        <Item nome="Uva" />
+        </>
+    )
+}
+export default ListaDeCompras
